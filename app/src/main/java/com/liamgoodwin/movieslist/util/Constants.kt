8728ce6f2435276f5559364d53fe11585d5406e0
Liam Goodwin <goodwin_liam@outlook.com>
2022-08-04 13:33:46 -0400

    Removed API key from Constants filegg

--- app/src/main/java/com/liamgoodwin/movieslist/util/Constants.kt
+++ app/src/main/java/com/liamgoodwin/movieslist/util/Constants.kt
@@ -2,6 +2,6 @@ package com.liamgoodwin.movieslist.util
 
 object Constants {
     const val OMDB_BASE_URL = "http://www.omdbapi.com"
-    const val OMDB_API_KEY = "2900301b"
+    const val OMDB_API_KEY = "your_api_key_here"
     const val PER_PAGE = 10
 }
